e7ca8466dca33af5350e7d9270dcb6bd12086330
seb1441 <sebastienauriault@gmail.com>
2020-06-21 09:22:10 -0400

    Add localization and new navbar

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module LabV3
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    I18n.available_locales = [:en, :fr]
   end
 end
