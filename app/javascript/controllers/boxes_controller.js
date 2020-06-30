a122ef0a682963d8b9eaae0e6ee10bea31a29001
seb1441 <sebastienauriault@gmail.com>
2020-06-30 08:41:04 -0400

    Fix boxes auto save and category names

--- app/javascript/controllers/boxes_controller.js
+++ app/javascript/controllers/boxes_controller.js
@@ -1,6 +1,7 @@
 import ApplicationController from './application_controller'
 import { debounce } from 'lodash-es'
 import serializeForm from 'form-serialize'
+import Rails from '@rails/ujs'
 
 export default class extends ApplicationController {
   static targets = [ "form", "boxDiv", "colorPicker", "richText", "name" ]
