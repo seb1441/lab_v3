a2fd88a339ca6f256563a976abcc4a16cfdb0c65
seb1441 <sebastienauriault@gmail.com>
2020-07-01 19:37:49 -0400

    Require box category

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,7 +1,7 @@
 class Box < ApplicationRecord
   belongs_to :user
   belongs_to :boxable, polymorphic: true, dependent: :destroy
-  belongs_to :box_category, required: false
+  belongs_to :box_category
 
   validates :name, presence: true
 
